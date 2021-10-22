01037df0cc200204cd14f676cfa6cbea2a0658c3
Ewerton Mendes <ewertonmendes9@gmail.com>
2021-10-22 20:58:40 -0300

    Adding App bar with changing text font

--- text-decorator/src/App.vue
+++ text-decorator/src/App.vue
@@ -1,56 +1,23 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <Header></Header>
     <v-main>
-      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Header from './components/layout/AppHeader';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Header,
+  },
+
+  mounted() {
+    console.log(this)
   },
 
   data: () => ({
@@ -58,3 +25,22 @@ export default {
   }),
 };
 </script>
+
+<style>
+@font-face {
+  font-family: "Retro";
+  src: local("fonts"),
+   url(./fonts/Retro-Stereo-Wide.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Creepy";
+  src: local("fonts"),
+   url(./fonts/HanaleiFill-Regular.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Comic";
+  src: local("fonts"),
+   url(./fonts/KOMIKAGL.ttf) format("truetype");
+}
+
+</style>
---
+++ text-decorator/src/components/layout/AppHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Text Decorator Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+
+      <h3 ref="titleText" class="description">Text Decorator</h3>
+    </div>
+
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  components: {},
+
+  data() {
+    return {
+      fontFamilies: ["Creepy", "Retro", "Helvetica", "Comic"],
+      lastIndex: 0,
+    };
+  },
+
+  mounted() {
+    this.changeFontFamily();
+  },
+
+  methods: {
+    generateRandom() {
+      var num = Math.floor(Math.random() * (this.fontFamilies.length - 1));
+      if(num === this.lastIndex) {
+        return this.generateRandom();
+      }
+      this.lastIndex = num;
+      return num;
+    },
+    changeFontFamily() {
+      setInterval(() => {
+        const index = this.generateRandom();
+        this.$refs.titleText.style.fontFamily = this.fontFamilies[index];
+      }, 3000);
+    },
+  },
+};
+</script>
+
+<style>
+.description {
+  font-family: "creepy";
+  font-size: 25px;
+}
+</style>
