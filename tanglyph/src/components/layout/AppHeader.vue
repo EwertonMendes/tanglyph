b20b88cba3c6b8ad06462ba87bb30c515f68cbb4
Ewerton Mendes <ewertonmendes9@gmail.com>
2021-11-21 20:08:22 -0300

    Adding logo

--- tanglyph/src/components/layout/AppHeader.vue
+++ tanglyph/src/components/layout/AppHeader.vue
@@ -5,9 +5,9 @@
         alt="Tanglyph Logo"
         class="shrink mr-2"
         contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        src="../../assets/logo.png"
         transition="scale-transition"
-        width="40"
+        width="60"
       />
 
       <h3>{{ title }}</h3>
