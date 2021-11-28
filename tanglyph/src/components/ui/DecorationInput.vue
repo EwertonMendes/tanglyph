753a9d3900b845b4f08659b1facbf27d84a629e2
Ewerton Mendes <ewertonmendes9@gmail.com>
2021-11-27 23:42:05 -0300

    Fixing messages

--- tanglyph/src/components/ui/DecorationInput.vue
+++ tanglyph/src/components/ui/DecorationInput.vue
@@ -15,49 +15,48 @@
     <v-divider class="pb-2"></v-divider>
     <v-text-field :value="fullTextValue" class="pt-3" readonly dense outlined>
       <template v-slot:append>
-        <v-row class="pt-2">
-          <decoration-modal
-            v-model="showModal"
-            :glyphName="glyphName"
-            @decorate="applyDecoration"
-          ></decoration-modal>
-          <v-tooltip top v-if="showDecorationButton">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                color="primary lighten-2"
-                plain
-                text
-                v-bind="attrs"
-                v-on="on"
-                @click.stop="showModal = true"
-              >
-                <v-icon color="orange">mdi-creation</v-icon>
-              </v-btn>
-            </template>
-            <span>{{$t('decoration-input.decorate-text')}}</span>
-          </v-tooltip>
-          <v-tooltip top v-if="showCopyButton">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                color="primary lighten-2"
-                plain
-                text
-                @click="copyText()"
-                v-bind="attrs"
-                v-on="on"
-              >
-                <v-icon>mdi-content-copy</v-icon>
-              </v-btn>
-            </template>
-            <span>{{$t('decoration-input.copy-to-clipboard')}}</span>
-          </v-tooltip>
-        </v-row>
+        <decoration-modal
+          v-model="showModal"
+          :glyphName="glyphName"
+          @decorate="applyDecoration"
+        ></decoration-modal>
+        <v-tooltip top v-if="showDecorationButton">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              class="pb-2"
+              color="primary lighten-2"
+              plain
+              text
+              v-bind="attrs"
+              v-on="on"
+              @click.stop="showModal = true"
+            >
+              <v-icon color="orange">mdi-creation</v-icon>
+            </v-btn>
+          </template>
+          <span>{{ $t("decoration-input.decorate-text") }}</span>
+        </v-tooltip>
+        <v-tooltip top v-if="showCopyButton">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              class="pb-2"
+              color="primary lighten-2"
+              plain
+              text
+              @click="copyText()"
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>mdi-content-copy</v-icon>
+            </v-btn>
+          </template>
+          <span>{{ $t("decoration-input.copy-to-clipboard") }}</span>
+        </v-tooltip>
       </template>
     </v-text-field>
   </div>
 </template>
 <script>
-
 import DecorationModal from "./DecorationModal";
 
 export default {
@@ -99,7 +98,7 @@ export default {
     fullTextValue: {
       get() {
         return this.glyphStyle.value;
-      }
+      },
     },
 
     // I had to put the replaceDecoration on vuex store because it didn't work as a data property
@@ -119,16 +118,19 @@ export default {
     },
     showToast() {
       this.$root.vtoast.show({
-        message: this.$t('decoration-input.copied', { msg: this.glyphStyle.value }),
+        message: this.$t("decoration-input.copied", {
+          msg: this.glyphStyle.value,
+        }),
         color: "success",
         icon: "mdi-check",
         timer: 3000,
       });
     },
     applyDecoration: function(decorationObj) {
-      
-      this.$store.dispatch('applyDecorationToStyleText', { decorationValueObj: decorationObj, glyphName: this.glyphName });    
-      
+      this.$store.dispatch("applyDecorationToStyleText", {
+        decorationValueObj: decorationObj,
+        glyphName: this.glyphName,
+      });
     },
   },
 };
