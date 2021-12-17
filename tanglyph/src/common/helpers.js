96a90408f5a22ef66e9b3211502253a1b146bb99
Ewerton Mendes <ewertonmendes9@gmail.com>
2021-12-17 20:24:50 -0300

    Update helpers.js

--- tanglyph/src/common/helpers.js
+++ tanglyph/src/common/helpers.js
@@ -1,22 +1,11 @@
 import glyphMaps from "./glyphMaps";
 
-function _normalizeLetter(letter) {
-    return letter
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "");
-}
-
 export default {
   convertTextToGlyph({ baseText, mapName }) {
     let newText = "";
     [...baseText].forEach((letter) => {
       const glyphLetter = glyphMaps.getLetter({letter, charMap: mapName});
-      if (!glyphLetter) {
-        const normalizedLetter = _normalizeLetter(letter)
-
-        return (newText +=
-          glyphMaps.getLetter({ letter: normalizedLetter.toLowerCase(), charMap: mapName }) ?? letter);
-      }
+      
       newText += glyphLetter;
     });
     return newText;
