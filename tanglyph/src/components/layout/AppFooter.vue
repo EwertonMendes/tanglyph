a28bd11ff65f5fdebb6308589b47ce1280f18b0d
Ewerton Mendes <86685761+Ewerton-Mendes@users.noreply.github.com>
2022-01-19 14:37:38 -0300

    Adds informaton panel

--- tanglyph/src/components/layout/AppFooter.vue
+++ tanglyph/src/components/layout/AppFooter.vue
@@ -1,50 +1,48 @@
 <template>
-    <v-footer padless>
-      <v-card
-        flat
-        tile
-        width="100%"
-        class="text-center"
-      >
-        <v-card-text>
-          <v-btn
-            class="mx-4"
-            icon
-            @click="toTop"
-          >
-            <v-icon size="24px">
-              mdi-home
-            </v-icon>
-          </v-btn>
-          <v-btn
-            class="mx-4"
-            icon
-            @click="openGithub"
-          >
-            <v-icon size="24px">
-              mdi-github
-            </v-icon>
-          </v-btn>
-        </v-card-text>
+  <v-footer padless>
+    <v-card flat tile width="100%" class="text-center">
+      <v-card-text>
+        <v-btn class="mx-4" icon @click="toTop">
+          <v-icon size="24px">
+            mdi-home
+          </v-icon>
+        </v-btn>
+        <v-btn class="mx-4" icon @click="openInformation">
+          <v-icon size="24px">
+            mdi-information
+          </v-icon>
+        </v-btn>
+        <v-btn class="mx-4" icon @click="openGithub">
+          <v-icon size="24px">
+            mdi-github
+          </v-icon>
+        </v-btn>
+      </v-card-text>
 
-        <v-divider></v-divider>
+      <v-divider></v-divider>
 
-        <v-card-text>
-          {{$t('footer.made-with-love')}} <strong>Ewerton Mendes</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
+      <v-card-text>
+        {{ $t("footer.made-with-love") }} <strong>Ewerton Mendes</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
-  export default {
-    methods: {
-        openGithub() {
-            window.open('https://github.com/EwertonMendes', '_blank').focus();
-        },
-        toTop() {
-            this.$vuetify.goTo(0);
-        }
-    }
-  }
+export default {
+  methods: {
+    openInformation() {
+      this.$root.$emit("open-help-modal", {
+        title: this.$t("about.title"),
+        content: this.$t("about.description"),
+      });
+    },
+    openGithub() {
+      window.open("https://github.com/EwertonMendes", "_blank").focus();
+    },
+    toTop() {
+      this.$vuetify.goTo(0);
+    },
+  },
+};
 </script>
